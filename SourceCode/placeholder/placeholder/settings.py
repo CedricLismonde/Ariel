04647ba928bd039375ce3e5dc3bea466e37673f3
Taylan Ünver <taylanunver@hotmail.com>
2024-01-18 19:52:21 +0100

    Migrate to supabase

--- SourceCode/placeholder/placeholder/settings.py
+++ SourceCode/placeholder/placeholder/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'placeholder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'A6m5eE4eVMMmKlwk',
+        'HOST': 'db.sstkuiznfhxhewtxpagq.supabase.co',
+        'PORT': '5432',
     }
 }
 
